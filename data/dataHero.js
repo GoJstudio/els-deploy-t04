7a3691bbc0c04ffdcfbdc03ab0bd4b1a9407a2a1
jwlee <lit20@daum.net>
2023-04-06 02:12:11 +0900

    new files of els-deploly-t04

---
+++ data/dataHero.js
@@ -0,0 +1,23 @@
+export const dataHero = [
+  {
+    idx: 1,
+    upperTitle: 'Uninterrupted power for',
+    lowerTitle: 'your uninterrupted business',
+    desc: 'Protect your business from power outages: With our power systems like UPS, your business can continue operating even during power outages. This means your employees can keep working, your customers can still make purchases, and your operations wont be disrupted.',
+    image: '/images/nasa-unsplash.jpg',
+  },
+  {
+    idx: 2,
+    upperTitle: 'Smart, reliable, and green',
+    lowerTitle: 'Solar Grid Technology has it all',
+    desc: 'Protect your business from power outages: With our power systems like UPS, your business can continue operating even during power outages. This means your employees can keep working, your customers can still make purchases, and your operations wont be disrupted.',
+    image: '/images/andres-siimon-unsplash.jpg',
+  },
+  {
+    idx: 3,
+    upperTitle: 'Smart Monitoring for',
+    lowerTitle: 'Optimal Efficiency and Performance',
+    desc: 'Protect your business from power outages: With our power systems like UPS, your business can continue operating even during power outages. This means your employees can keep working, your customers can still make purchases, and your operations wont be disrupted.',
+    image: '/images/ernest-ojeh-unsplash.jpg',
+  },
+]
---
+++ data/dataWorks.js
@@ -0,0 +1,20 @@
+export const dataWorks = [
+  {
+    idx: 1,
+    date: 'Nov 15, 2022',
+    title: 'Saving your energy from the roof',
+    picture: 'anders-j-unsplash.jpg',
+  },
+  {
+    idx: 2,
+    date: 'Jun 23, 2022',
+    title: 'Most stable power in data center',
+    picture: 'jordan-harrison-unsplash.jpg',
+  },
+  {
+    idx: 3,
+    date: 'Mar 09, 2022',
+    title: 'Efficient energy for a sustainable future',
+    picture: 'andrey-metelev-unsplash.jpg',
+  },
+]
